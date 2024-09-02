a87dc92db74afaa30fe66ae4481d46a463bcaf76
Jonnatan Farias <111089145+JonnatanFarias@users.noreply.github.com>
2024-09-02 00:47:58 -0300

    finalizado

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const end = Date.now() + 15 * 5000;
+
+// go Buckeyes!
+const colors = ["#212121", "#ffffff"];
+
+(function frame() {
+  confetti({
+    particleCount: 2,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 },
+    colors: colors,
+  });
+
+  confetti({
+    particleCount: 2,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 },
+    colors: colors,
+  });
+
+  if (Date.now() < end) {
+    requestAnimationFrame(frame);
+  }
+})();
